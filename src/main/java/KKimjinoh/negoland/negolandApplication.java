b779247102e803a232a651c8ca801d70823221be
urim <jiurim0316@naver.com>
2024-02-13 17:11:35 +0900

    Feat: Spring 프로ì프로젝트 생성 및 초기 세팅

---
+++ src/main/java/KKimjinoh/negoland/negolandApplication.java
@@ -0,0 +1,13 @@
+package KKimjinoh.negoland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class negolandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(negolandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KKimjinoh/negoland/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package KKimjinoh.negoland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
